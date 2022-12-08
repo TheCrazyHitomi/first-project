b6c0cc450bcd8538bb2886be1284e33c0890c4a0
TheCrazyHitomi <amandine-tomas@hotmail.fr>
2022-12-08 22:03:20 +0100

    correctif CSS menu burger

--- src/script.js
+++ src/script.js
@@ -1,22 +1,32 @@
-var app = (function () {
-	var body = undefined;
-	var menu = undefined;
-	var menuItems = undefined;
-	var init = function init() {
-		body = document.querySelector("body");
-		menu = document.querySelector(".menu-icon");
-		menuItems = document.querySelector(".nav_list-item");
-		applyListeners();
-	};
-	var applyListeners = function applyListeners() {
-		menu.addEventListener("click", function () {
-			return toggleClass(body, "nav-active");
-		});
-	};
-	var toggleClass = function toggleClass(element, stringClass) {
-		if (element.classList.contains(stringClass))
-			element.classList.remove(stringClass);
-		else element.classList.add(stringClass);
-	};
-	init();
-})();
+const burger = document.querySelector(".burger");
+
+const burgerMenu = document.createElement("div");
+const lineUp = document.createElement("div");
+const lineMiddle = document.createElement("div");
+const lineDown = document.createElement("div");
+// const globalMenu = document.querySelector(".nav");
+
+// console.log(globalMenu);
+
+burgerMenu.appendChild(lineUp);
+burgerMenu.appendChild(lineMiddle);
+burgerMenu.appendChild(lineDown);
+
+burgerMenu.classList.add("burger-menu");
+lineUp.classList.add("line-up");
+lineMiddle.classList.add("line-middle");
+lineDown.classList.add("line-down");
+
+burger.appendChild(burgerMenu);
+const globalMenu = document.querySelector(".nav");
+
+burgerMenu.addEventListener("click", () => {
+	if (globalMenu.classList.contains("visible")) {
+		globalMenu.classList.remove("visible");
+	} else {
+		globalMenu.classList.add("visible");
+	}
+});
+
+console.log(globalMenu);
+// burger.appendChild(burgerMenu);
